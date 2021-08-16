90b4882fc0d2d50dd9ba3c271e5aa8c5d1e2970e
take907 <taketake63742@gmail.com>
2021-08-16 23:00:53 +0900

    add_bookshelf_database

--- main.py
+++ main.py
@@ -1,1 +1,36 @@
-import os
+import tkinter as tk
+import sqlite3
+from tkinter.filedialog import askopenfile
+from tkinter.messagebox import askquestion
+
+root=tk.Tk()
+root.geometry("300x300")
+# ans=askquestion("Do you want to open database, or Create Database?")
+
+
+
+dbname = 'myshelf.sqlite3'
+# DBを作成する（既に作成されていたらこのDBに接続する）
+conn = sqlite3.connect(dbname)
+
+cur = conn.cursor()
+
+# テーブルの作成
+# cur.execute(
+#     'CREATE TABLE mybooks(id INTEGER PRIMARY KEY AUTOINCREMENT, name STRING)'
+# )
+cur.execute('INSERT INTO mybooks values(2,"Classic Python")')
+# cur.execute("DELETE FROM items WHERE id='1'")
+cur.execute("SELECT * FROM mybooks")
+
+for row in cur:
+    print(row)
+
+# コミットしないと登録が反映されない
+conn.commit()
+
+# DBとの接続を閉じる(必須)
+conn.close()
+
+
+# root.mainloop()
